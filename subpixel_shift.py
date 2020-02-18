f14fa06e9904886bca1a9742fbbce0dfbedb628d
Kerry Paterson <kerry@dhcp-165-124-148-118.ciera.northwestern.edu>
2020-02-18 16:43:26 -0600

    Removed the first example of performing subpixel shifts for the built-in python function and added an example of how to measure the shift with image registration.

--- subpixel_shift.py
+++ subpixel_shift.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python
 
-"Python script showing how to perform subpixel shifts of data in python."
+"Python script showing how to perform and measure subpixel shifts of data in python."
 "Author: Kerry Paterson"
 
 import numpy as np
 import matplotlib.pyplot as plt
-
-def convertfreqs(x): #function to convert x to frequency
-    return np.mod((x + N/2),N) - N/2
-
-def fft(x): #function to perform real part of fft
-    return np.fft.fft2(x)
-
-def ifft(x): #function to perform imaginary part of fft
-    return np.fft.ifft2(x) 
+import scipy
+from skimage.feature import register_translation
 
 N = 30 #N needs to larger than the size of the and the shift you want to apply
 
@@ -27,12 +20,7 @@ data = np.exp(-((d)**2/(2.0*0.5**2))) #create 2D guassian for example
 f = np.zeros((N,N)) #create a new array with the new size for the shifted data
 f[:data.shape[0],:data.shape[1]] = data[:,:] #add the data to the new array
 
-x = np.tile(np.arange(N),(N,1)).T #create x for shifted data
-x = convertfreqs(x) #convert x to frequencies
-y = x.T #create y for shifted data
-
-shifted = ifft(fft(f)*np.exp(1j*2*np.pi*(-xshift*x/N-yshift*y/N))) #calculate fft of the data with the shift
-shifted = np.abs(shifted) #use only the real part
+shifted = np.fft.ifft2(scipy.ndimage.fourier_shift(np.fft.fft2(f),(2.5,1.5))).real #shift data using FFTs
 
 plt.figure() #plot to show the original data and the shifted data
 plt.subplot(121)
@@ -43,18 +31,13 @@ plt.imshow(shifted, interpolation='none')
 plt.title('Shifted data')
 plt.show()
 
-#another way is to use scipy.ndimage.fourier_shift:
-#this doesn't require you to define a new x, y grid
-#but you still need to use f instead of data otherwise
-#the data will wrap around to the other edge.
+#how to measure the shift is to use image registration:
+n = np.random.normal(0,0.1,900).reshape(30,30) #create some noise
+f = f+n #add the noise to the original data to simulate real data
 
-shifted = =ifft(scipy.ndimage.fourier_shift(fft(f),[2.5,1.5])).real 
+n = np.random.normal(0,0.1,900).reshape(30,30) #create some noise
+shifted = shifted+n #add the noise to the shifted data to make it slightly different to the original
 
-plt.figure() #plot to show the original data and the shifted data
-plt.subplot(121)
-plt.imshow(f, interpolation='none')
-plt.title('Original data')
-plt.subplot(122)
-plt.imshow(shifted, interpolation='none')
-plt.title('Shifted data')
-plt.show()
+shift, error, diffphase = register_translation(shifted,f,100) #the 100 gives subpixel measurements
+
+print('The measured shift between data is x = '+str(shift[0])+' and y = '+str(shift[1])) #without the noise the measured shifted = the actual shift
